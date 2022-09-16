457638241a85aaefaa27ab6b5e7bd56a82d833d4
Graham Booth <graham.r.booth@gmail.com>
2022-09-16 17:55:39 +0200

    first attempt ex00

--- 4_c++/0/ex00/megaphone.cpp
+++ 4_c++/0/ex00/megaphone.cpp
@@ -3,19 +3,29 @@
 int main(int argc, char **argv)
 {
 	int i;
+	int j;
 	char *input_string;
 
-	i = 0;
-	input_string = argv[1];
-	if (argc == 2)
+	if (argc >= 2)
 	{
-		while(input_string[i])
+		i = 1;
+		while(argv[i])
 		{
-			if (input_string[i] >= 'a' && input_string[i] <= 'z')
-				input_string[i] = input_string[i] - 32;
-			std::cout << input_string[i];
+			input_string = argv[i];
+			j = 0;
+			while(input_string[j])
+			{
+				if (input_string[j] >= 'a' && input_string[j] <= 'z')
+					input_string[j] = input_string[j] - 32;
+				std::cout << input_string[j];
+				j++;
+			}
 			i++;
 		}
 	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << '\n';
+
 	return (0);
 }
