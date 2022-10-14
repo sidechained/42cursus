61feb41f4b0fa565d5cde2db15cb199d5381edeb
Graham Booth <graham.r.booth@gmail.com>
2022-10-14 19:17:17 +0200

    ls

--- 4_c++/2/ex00/main.cpp
+++ 4_c++/2/ex00/main.cpp
@@ -6,26 +6,16 @@
 /*   By: grm <grm@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 08:10:28 by grm               #+#    #+#             */
-/*   Updated: 2022/09/28 08:48:47 by grm              ###   ########.fr       */
+/*   Updated: 2022/10/14 19:16:29 by grm              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// To define a fixed point type conceptually, all we need are two parameters:
-// - width of the number representation, and
-// - binary point position within the number
-
-// From now on, each class you write MUST be in orthodox canonical form:
-// - at least one default constructor
-// - a copy contructor
-// - an assignation operator overload
-// - a destructor
-
 #include "Fixed.hpp"
 
-int main( void )
+int main(void)
 {
 	Fixed a;
-	Fixed b( a );
+	Fixed b(a);
 	Fixed c;
 
 	c = b;
